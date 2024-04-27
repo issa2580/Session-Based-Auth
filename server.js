e3bbb4ce36580cc718fd5473529788e2eaade992
issa2580 <issadiop2580@gmail.com>
2024-04-27 01:44:05 +0000

    add connection route

--- server.js
+++ server.js
@@ -20,6 +20,20 @@ app.use(
   })
 );
 
+// Connexion route
+app.post("signin", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (user) {
+    req.session.userId = user.id;
+    res.send("Login successfully");
+  } else {
+    res.status(401).send("Invalid credentials");
+  }
+});
+
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
